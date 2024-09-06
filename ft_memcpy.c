53aa2856845936b374d6447034ba518eca790dff
Matej Hegedus <mhegedus@c3r10p7.42vienna.com>
2024-09-06 19:16:46 +0200

    memcpy

---
+++ ft_memcpy.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhegedus <mhegedus@student.42vienna.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/06 16:03:56 by mhegedus          #+#    #+#             */
+/*   Updated: 2024/09/06 19:09:09 by mhegedus         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		((char *)dest)[i] = ((char *)src)[i];
+		i++;
+	}
+	return (dest);
+}
+/*
+#include <stdio.h>
+int main(void)
+{
+	char src[] = "ABCDEFGHIJKLMNOPQ";
+	char dest[] = "alphabet!";
+	printf("%s", (char *)ft_memcpy(dest, src, 4));
+}
+*/
