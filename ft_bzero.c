f372d46c73d7511c932f389bd4ce7b55cd615560
Matej Hegedus <mhegedus@c3r10p7.42vienna.com>
2024-09-06 16:11:28 +0200

    bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,34 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mhegedus <mhegedus@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/05 18:03:36 by mhegedus          #+#    #+#             */
-/*   Updated: 2024/09/06 16:06:23 by mhegedus         ###   ########.fr       */
+/*   Created: 2024/09/06 16:03:56 by mhegedus          #+#    #+#             */
+/*   Updated: 2024/09/06 16:06:07 by mhegedus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
-
-void	*ft_memset(void *s, int c, size_t n)
+void	bzero(void *s, size_t n)
 {
 	size_t	i;
 
 	i = 0;
 	while (i < n)
 	{
-		((char *)s)[i] = c;
+		((char *)s)[i] = '/0';
 		i++;
 	}
 	return (s);
 }
-/*
-#include <stdio.h>
-int main(void)
-{
-	char str[] = "alphabet!";
-	printf("%s", (char *)ft_memset(str, 'Y', 4));
-}
-*/
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mhegedus <mhegedus@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 17:18:46 by mhegedus          #+#    #+#             */
-/*   Updated: 2024/09/05 19:30:24 by mhegedus         ###   ########.fr       */
+/*   Updated: 2024/09/06 16:03:20 by mhegedus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,18 @@ int		ft_isascii(int c);
 int		ft_isprint(int c);
 size_t	ft_strlen(const char *s);
 void	*memset(void *s, int c, size_t n);
-int		ft_bzero(int c);
-int		ft_memcpy(int c);
-int		ft_memmove(int c);
-int		ft_strlcpy(int c);
-int		ft_strlcat(int c);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	ft_memmove(void *dest, const void *src, size_t n);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+// toupper
+// ft_tolower
+// ft_strchr
+// ft_strrchr
+// ft_strncmp
+// ft_memchr
+// ft_memcmp
+// ft_strnstr
+// ft_atoi
 #endif
