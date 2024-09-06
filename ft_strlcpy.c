d097a5d276814c6f7fd84821ed91006450a66237
Matej Hegedus <mhegedus@c3r10p7.42vienna.com>
2024-09-06 20:49:41 +0200

    some unfinished functions

---
+++ ft_strlcpy.c
@@ -0,0 +1,48 @@
+unsigned int	str_len(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (i < size - 1 && src[i])
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (str_len(src));
+}
+
+/*
+#include <stdio.h>
+int	main(void)
+{
+	char src[] = "String1!";
+	char dest[] = "A longer string.";
+	
+	printf("src: %s\ndest: %s\n", src, dest);
+	ft_strlcpy(dest, src, 5);
+	printf("src: %s\ndest: %s\n", src, dest);
+	ft_strlcpy(dest, src, 9);
+    printf("src: %s\ndest: %s\n", src, dest);
+	ft_strlcpy(dest, src, 12);
+    printf("src: %s\ndest: %s\n", src, dest);
+	int i = 0;
+	while (i < 16)
+	{
+		printf("%c", dest[i]);
+		i++;
+	}
+}
+*/
