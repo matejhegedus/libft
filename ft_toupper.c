95fe4c95fa884c318d7e1352b6adb9a7b230bfec
Matej Hegedus <mhegedus@c3r10p7.42vienna.com>
2024-09-19 20:08:36 +0200

    toupper

---
+++ ft_toupper.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_toupper.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhegedus <mhegedus@student.42vienna.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/19 19:55:42 by mhegedus          #+#    #+#             */
+/*   Updated: 2024/09/19 20:08:11 by mhegedus         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_toupper(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (c - 32);
+	else
+		return (c);
+}
+/*
+#include <stdio.h>
+int main(int argc, char **argv)
+{
+	if (argc == 2)
+	{
+		printf("%c %c", argv[1][0], (char)ft_toupper(argv[1][0]));
+	}
+}
+*/
