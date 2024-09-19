1c5b10abdd4f3c444319bd3825e4d003048340c8
Matej Hegedus <mhegedus@c3r10p7.42vienna.com>
2024-09-19 21:39:54 +0200

    memcmp

---
+++ ft_memcmp.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhegedus <mhegedus@student.42vienna.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/19 21:19:40 by mhegedus          #+#    #+#             */
+/*   Updated: 2024/09/19 21:39:39 by mhegedus         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t			i;
+	unsigned char	*s1_uc;
+	unsigned char	*s2_uc;
+
+	if (n == 0)
+		return (0);
+	s1_uc = (unsigned char *)s1;
+	s2_uc = (unsigned char *)s2;
+	i = 0;
+	while (i < n)
+	{
+		if (s1_uc[i] - s2_uc[i] != 0)
+			return (s1_uc[i] - s2_uc[i]);
+		i++;
+	}
+	return (0);
+}
+/*
+#include <stdio.h>
+int	main(void)
+{
+	printf("1. %d\n", ft_memcmp("String", "String", 3));
+	printf("2. %d\n", ft_memcmp("String", "StrABCDEFGH", 3));
+	printf("3. %d\n", ft_memcmp("SZring", "String", 3));
+	printf("4. %d\n", ft_memcmp("String", "", 3));
+	printf("5. %d\n", ft_memcmp("", "String", 3));
+	printf("6. %d\n", ft_memcmp("\n", "\b", 3));
+	printf("7. %d\n", ft_memcmp("String", "String", 0));
+
+	char s1[] = "r";
+	char s3[] = "abcdefgh";
+	char s2[] = "r";
+	printf("8. %d\n", ft_memcmp(s1, s2, 1));
+	printf("9. %d\n", ft_memcmp(s3, s2, 1));
+}
+*/
