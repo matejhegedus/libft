59c8df5a42b5b1baf5c60116b20e3958f30d0c20
Matej Hegedus <mhegedus@c3r10p7.42vienna.com>
2024-09-19 14:33:55 +0200

    bzero, memcpy, strlcpy

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,19 +6,30 @@
 /*   By: mhegedus <mhegedus@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/06 16:03:56 by mhegedus          #+#    #+#             */
-/*   Updated: 2024/09/06 16:06:07 by mhegedus         ###   ########.fr       */
+/*   Updated: 2024/09/19 14:30:19 by mhegedus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	bzero(void *s, size_t n)
+#include <stddef.h>
+
+void	ft_bzero(void *s, size_t n)
 {
 	size_t	i;
 
 	i = 0;
 	while (i < n)
 	{
-		((char *)s)[i] = '/0';
+		((char *)s)[i] = '\0';
 		i++;
 	}
-	return (s);
 }
+
+// #include <stdio.h>
+// int	main(void)
+// {
+// 	char	str[4] = "Nice";
+//	
+// 	printf("Before: %s\n", str);
+// 	ft_bzero(str, 4);
+// 	printf("After: %s\n", str);
+// }
