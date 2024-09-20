59d87c80598185fa08f0586f30ccaf27da5a8ff5
Matej Hegedus <mhegedus@c3r10p7.42vienna.com>
2024-09-20 13:47:35 +0200

    strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: mhegedus <mhegedus@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 13:25:27 by mhegedus          #+#    #+#             */
-/*   Updated: 2024/09/20 13:35:49 by mhegedus         ###   ########.fr       */
+/*   Updated: 2024/09/20 13:47:23 by mhegedus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,11 @@
 #include <errno.h>
 #include "libft.h"
 
-char *ft_strdup(const char *s)
+char	*ft_strdup(const char *s)
 {
-	size_t len;
-	char *new_s;
+	size_t	len;
+	char	*new_s;
+	size_t	i;
 
 	len = ft_strlen(s);
 	new_s = malloc(sizeof(char) * (len + 1));
@@ -26,5 +27,22 @@ char *ft_strdup(const char *s)
 		errno = ENOMEM;
 		return (NULL);
 	}
-	
+	i = 0;
+	while (i < len)
+	{
+		new_s[i] = s[i];
+		i++;
+	}
+	new_s[i] = '\0';
+	return (new_s);
 }
+/*
+#include <stdio.h>
+int main(void)
+{
+	const char *s = "  This is a test String.";
+	char *new_s = ft_strdup(s);
+
+	printf("%s", new_s);
+}
+*/
