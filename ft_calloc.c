a0ffd85b65a915eb56846e090c66a7a804b76488
Matej Hegedus <mhegedus@c3r10p7.42vienna.com>
2024-09-20 13:25:09 +0200

    ft_calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: mhegedus <mhegedus@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 09:55:36 by mhegedus          #+#    #+#             */
-/*   Updated: 2024/09/20 11:23:36 by mhegedus         ###   ########.fr       */
+/*   Updated: 2024/09/20 13:24:48 by mhegedus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,42 @@
 
 // returns 1 if multiplying the 2 numbers results in an overflow
 // else returns 0
-static char	is_overflow(size_t nmemb, size_t size)
+static char	is_multiply_overflow(size_t nmemb, size_t size)
 {
+	if (nmemb > 0 && size > __SIZE_MAX__ / nmemb)
+		return (1);
+	if (size > 0 && nmemb > __SIZE_MAX__ / size)
+		return (1);
+	return (0);
 }
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*mem;
+	size_t	i;
 
-	is_overflow(size_t nmemb, size_t size);
+	if (nmemb == 0 || is_multiply_overflow(nmemb, size))
+		return (NULL);
 	mem = malloc(nmemb * size);
 	if (mem == NULL)
 		return (NULL);
+	i = 0;
+	while (i < nmemb * size)
+	{
+		((char *)mem)[i] = 0;
+		i++;
+	}
+	return (mem);
 }
+/*
+#include <stdio.h>
+int main(void)
+{
+	int *arr;
+	arr = ft_calloc(sizeof(int), 12);
+	for (int i = 0; i < 12; i++)
+	{
+		printf("%d\n", arr[i]);
+	}
+}
+*/
