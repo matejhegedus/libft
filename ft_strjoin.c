df275122fa664a7801f902a172a3d407990dabd5
Matej Hegedus <mhegedus@c3r10p7.42vienna.com>
2024-09-25 14:07:42 +0200

    strjoin and added all function declarations from part 2 to header file

---
+++ ft_strjoin.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhegedus <mhegedus@student.42vienna.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/25 13:55:02 by mhegedus          #+#    #+#             */
+/*   Updated: 2024/09/25 14:06:48 by mhegedus         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*result;
+	size_t	s1_len;
+	size_t	i;
+
+	s1_len = ft_strlen(s1);
+	result = malloc(s1_len + ft_strlen(s2) + 1);
+	if (result == NULL)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		result[i] = s1[i];
+		i++;
+	}
+	while (s2[i - s1_len])
+	{
+		result[i] = s2[i - s1_len];
+		i++;
+	}
+	result[i] = '\0';
+	return (result);
+}
+/*
+#include <stdio.h>
+int main(void)
+{
+	const char *s1 = "first";
+	const char *s2 = "second";
+
+	printf("%s", ft_strjoin(s1, s2));
+}
+*/
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mhegedus <mhegedus@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 17:18:46 by mhegedus          #+#    #+#             */
-/*   Updated: 2024/09/25 13:06:02 by mhegedus         ###   ########.fr       */
+/*   Updated: 2024/09/25 13:57:10 by mhegedus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,4 +39,14 @@ void	*ft_calloc(size_t nmemb, size_t size);
 char	*strdup(const char *s);
 
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
+char	*ft_itoa(int n);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 #endif
