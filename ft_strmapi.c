1a6ed92ce6ae777f3c9ac48610cfdb28f97fd881
matejhegedus <matejhegedus@gmail.com>
2024-10-01 21:39:35 +0200

    strmapi

---
+++ ft_strmapi.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhegedus <mhegedus@student.42vienna.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/01 21:09:54 by mhegedus          #+#    #+#             */
+/*   Updated: 2024/10/01 21:38:19 by mhegedus         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	size_t	result_len;
+	char	*result;
+	size_t	i;
+
+	result_len = ft_strlen(s);
+	result = malloc(result_len + 1);
+	if (result == NULL)
+		return (NULL);
+	i = 0;
+	while (i < result_len)
+	{
+		result[i] = f(i, s[i]);
+		i++;
+	}
+	result[i] = '\0';
+	return (result);
+}
+/*
+#include <stdio.h>
+char	test_f(unsigned int index, char c)
+{
+	char result;
+
+	result = c + index;
+	return (result);
+}
+
+int	main(void)
+{
+	printf("%s\n", ft_strmapi("This is a test string", test_f));	
+}
+*/
