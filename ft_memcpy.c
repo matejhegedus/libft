c4d6318986639aaaaf4aacec9514da86a3bd512f
matejhegedus <matejhegedus@gmail.com>
2024-10-08 01:30:13 +0200

    fix memcpy according to libft unit test

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: mhegedus <mhegedus@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/06 16:03:56 by mhegedus          #+#    #+#             */
-/*   Updated: 2024/09/19 14:33:26 by mhegedus         ###   ########.fr       */
+/*   Updated: 2024/10/08 01:28:17 by mhegedus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t	i;
 
+	if (dest == NULL && src == NULL)
+		return (NULL);
 	i = 0;
 	while (i < n)
 	{
