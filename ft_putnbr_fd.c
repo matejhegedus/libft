8c4e43e9e97f09c832868993da0d45472bd82af5
matejhegedus <matejhegedus@gmail.com>
2024-10-08 01:11:25 +0200

    fixed putnbr fd accoirding to warmachine

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,25 +6,36 @@
 /*   By: mhegedus <mhegedus@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 22:00:21 by mhegedus          #+#    #+#             */
-/*   Updated: 2024/10/01 22:17:04 by mhegedus         ###   ########.fr       */
+/*   Updated: 2024/10/08 01:08:33 by mhegedus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include "libft.h"
+#include <stdbool.h>
+
+static int	to_positive(int n)
+{
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
+}
+
+static void	write_number_recursive(int n, int fd)
+{
+	char	c;
+
+	if (n / 10)
+		write_number_recursive(n / 10, fd);
+	c = to_positive(n % 10) + '0';
+	write(fd, &c, 1);
+}
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	size_t	i;
-	char	*s;
-
-	i = 0;
-	s = ft_itoa(n);
-	while (s[i])
-	{
-		write(fd, &(s[i]), 1);
-		i++;
-	}
+	if (n < 0)
+		write(fd, "-", 1);
+	write_number_recursive(n, fd);
 }
 /*
 #include <stdlib.h>
