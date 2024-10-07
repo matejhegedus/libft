3e302bba88308865e7e409b0e1331d39cb9faa7d
matejhegedus <matejhegedus@gmail.com>
2024-10-08 00:42:45 +0200

    fix ft_split according to warmachine

--- ft_split.c
+++ ft_split.c
@@ -6,11 +6,12 @@
 /*   By: mhegedus <mhegedus@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/28 12:55:15 by mhegedus          #+#    #+#             */
-/*   Updated: 2024/10/01 22:44:16 by mhegedus         ###   ########.fr       */
+/*   Updated: 2024/10/08 00:42:22 by mhegedus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include "libft.h"
 
 static size_t	count_words(char const *s, char delimiter)
 {
@@ -44,39 +45,30 @@ static size_t	get_word_len(char const *s, char delimiter)
 	return (i);
 }
 
-static char const	*skip_delimiter(char const *s, char c)
+static char const	*skip_delimiter(char const *s, char delimiter)
 {
-	while (*s == c)
+	while (*s == delimiter)
 		s++;
 	return (s);
 }
 
 static void	fill_word_array(char **result, char const *s, size_t num_of_words,
-		char c)
+		char delimiter)
 {
 	size_t	i;
-	size_t	j;
 	size_t	word_len;
 
-	s = skip_delimiter(s, c);
+	s = skip_delimiter(s, delimiter);
 	i = 0;
 	while (i < num_of_words)
 	{
-		word_len = get_word_len(s, c);
-		result[i] = malloc(word_len * sizeof(char));
-		j = 0;
-		while (j < word_len)
-		{
-			result[i][j] = s[j];
-			j++;
-		}
+		word_len = get_word_len(s, delimiter);
+		result[i] = ft_substr(s, 0, word_len);
 		s = s + word_len;
-		s = skip_delimiter(s, c);
-		result[i][j] = '\0';
+		s = skip_delimiter(s, delimiter);
 		i++;
 	}
-	result[i] = malloc(1 * sizeof(char));
-	result[i][0] = '\0';
+	result[i] = NULL;
 }
 
 char	**ft_split(char const *s, char c)
@@ -95,9 +87,10 @@ int	main(void)
 {
 	char	**arr;
 
-	arr = ft_split("  This is a    String   ", ' ');
+	arr = ft_split("lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+Sed non risus. Suspendisse", ' ');
 	int i = 0;
-	while (arr[i][0])
+	while (arr[i])
 	{
 		printf("%s\n", arr[i]);
 		i++;
